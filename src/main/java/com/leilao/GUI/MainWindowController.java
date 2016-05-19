ee34e0112a752df4a2f4783150ac099fcffdd9dd
Arthur Vedana <agieselvedana@gmail.com>
2016-05-19 20:09:51 -0300

    Lista agora carrega da base de dados (acho)

--- src/main/java/com/leilao/GUI/MainWindowController.java
+++ src/main/java/com/leilao/GUI/MainWindowController.java
@@ -1,9 +1,12 @@
 package com.leilao.GUI;
 
 import com.leilao.entidades.Lote;
+import com.leilao.servicos.ServicoImovel;
+import com.leilao.servicos.ServicoLote;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.HBox;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Created by Arthur on 19/05/2016.
@@ -14,10 +17,18 @@ public class MainWindowController {
 
     @FXML private ListView<Lote> loteListView;
 
+    @Autowired
+    private ServicoLote servicoLote;
+    @Autowired
+    private ServicoImovel servicoImovel;
+
     @FXML
     private void initialize() {
 
         loggedButtonBar.visibleProperty().bind(unloggedButtonBar.visibleProperty().not());
+
+        loteListView.getItems().setAll(servicoLote.findAll());
+        loteListView.getItems().addAll(servicoImovel.findAll());
 
         loteListView.setCellFactory(t -> new LoteCell());
 
