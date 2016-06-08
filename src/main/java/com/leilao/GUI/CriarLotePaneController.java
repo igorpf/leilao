7021e169b14c5a998c223c28912957c5af34616f
Arthur Vedana <agieselvedana@gmail.com>
2016-06-07 23:49:53 -0300

    Adicionada validação para criação de lotes

--- src/main/java/com/leilao/GUI/CriarLotePaneController.java
+++ src/main/java/com/leilao/GUI/CriarLotePaneController.java
@@ -6,19 +6,25 @@ import com.leilao.entidades.Lote;
 import com.leilao.entidades.Usuario;
 import com.leilao.servicos.ServicoImovel;
 import com.leilao.servicos.ServicoLote;
+import javafx.beans.property.StringProperty;
+import javafx.beans.value.ChangeListener;
 import javafx.collections.ListChangeListener;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
+import javafx.util.converter.NumberStringConverter;
 import org.controlsfx.control.SegmentedButton;
 import org.controlsfx.validation.ValidationResult;
 import org.controlsfx.validation.ValidationSupport;
+import org.controlsfx.validation.Validator;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Consumer;
 
 /**
@@ -36,6 +42,8 @@ public class CriarLotePaneController {
 
     @FXML private Label areaLabel, banheirosLabel, quartosLabel;
     @FXML private TextField areaField, banheirosField, quartosField;
+
+    @FXML private Button terminarButton;
 
     private Usuario owner;
     private Consumer<Lote> onFinish;
@@ -57,19 +65,82 @@ public class CriarLotePaneController {
         tipo2.setUserData(Imovel.class);
         tipoButton.getButtons().setAll(tipo1, tipo2);
 
-        ToggleButton subtipo1 = new ToggleButton("Residencial");
-        subtipo1.setUserData(Imovel.Tipo.Residencial);
-        ToggleButton subtipo2 = new ToggleButton("Comercial");
-        subtipo2.setUserData(Imovel.Tipo.Comercial);
+        addPermaSelectionFilter(tipoButton.getToggleGroup(), tipo1, tipo2);
+
+        ToggleButton subtipo1 = new ToggleButton("Comercial");
+        subtipo1.setUserData(Imovel.Tipo.Comercial);
+        ToggleButton subtipo2 = new ToggleButton("Residencial");
+        subtipo2.setUserData(Imovel.Tipo.Residencial);
         subtipoButton.getButtons().setAll(subtipo1, subtipo2);
+
+        addPermaSelectionFilter(subtipoButton.getToggleGroup(), subtipo1, subtipo2);
 
         subtipo2.prefWidthProperty().bindBidirectional(subtipo1.prefWidthProperty());
         tipo1.prefWidthProperty().bindBidirectional(subtipo1.prefWidthProperty());
         tipo2.prefWidthProperty().bindBidirectional(subtipo1.prefWidthProperty());
 
         subtipo1.setPrefWidth(100);
-
         subtipoButton.visibleProperty().bind(tipo2.selectedProperty());
+        quartosField.visibleProperty().bind(tipo2.selectedProperty().and(subtipo2.selectedProperty()));
+
+        bindVisiblity();
+
+        restrictFields();
+
+        ValidationSupport val = new ValidationSupport();
+
+        val.validationResultProperty().addListener(((observable, oldValue, newValue) -> {
+            terminarButton.setDisable(!newValue.getErrors().isEmpty());
+        }));
+
+        val.registerValidator(nomeField, Validator.createEmptyValidator("É preciso inserir um nome."));
+
+        val.registerValidator(lanceField, (c, newString) ->
+                ValidationResult.fromErrorIf(c, "É preciso informar o lance mínimo",
+                        !hasValidFloat((String) newString)));
+
+        val.registerValidator(areaField, (c, newString) ->
+                ValidationResult.fromErrorIf(c, "É necessário informar uma área",
+                        tipo2.isSelected() && !hasValidFloat((String) newString)));
+
+        val.registerValidator(banheirosField, (c, newString) ->
+                ValidationResult.fromErrorIf(c, "É necessário informar a quantidade de banheiros",
+                        tipo2.isSelected() && newString.equals("")));
+
+        val.registerValidator(quartosField, (c, newString) ->
+                ValidationResult.fromErrorIf(c, "É necessário a quantidade de quartos",
+                        tipo2.isSelected() && subtipo2.isSelected() && newString.equals("")));
+
+        tipo2.selectedProperty().addListener(((observable, oldValue, newValue) -> {
+            areaField.setText(newValue ? "" : "1");
+            banheirosField.setText(newValue ? "" : "1");
+        }));
+
+        subtipo2.selectedProperty().addListener(((observable, oldValue, newValue) -> {
+            quartosField.setText(newValue ? "" : "1");
+        }));
+
+        tipo1.setSelected(true);
+        subtipo1.setSelected(true);
+
+        areaField.setText("1");
+        banheirosField.setText("1");
+        quartosField.setText("1");
+    }
+
+    private void addPermaSelectionFilter(ToggleGroup group, ToggleButton... buttons) {
+        for (ToggleButton button : buttons)
+            button.addEventFilter(MouseEvent.MOUSE_RELEASED, new EventHandler<MouseEvent>() {
+                @Override public void handle(MouseEvent mouseEvent) {
+                    if (button.equals(group.getSelectedToggle())) {
+                        mouseEvent.consume();
+                    }
+                }
+            });
+    }
+
+    private void bindVisiblity() {
+
         subtipoButton.managedProperty().bind(subtipoButton.visibleProperty());
 
         areaField.visibleProperty().bind(subtipoButton.visibleProperty());
@@ -84,11 +155,52 @@ public class CriarLotePaneController {
         banheirosLabel.visibleProperty().bind(banheirosField.visibleProperty());
         banheirosLabel.managedProperty().bind(banheirosLabel.visibleProperty());
 
-        quartosField.visibleProperty().bind(subtipoButton.visibleProperty().and(subtipo1.selectedProperty()));
         quartosField.managedProperty().bind(quartosField.visibleProperty());
 
         quartosLabel.visibleProperty().bind(quartosField.visibleProperty());
         quartosLabel.managedProperty().bind(quartosLabel.visibleProperty());
+    }
+
+    private void restrictFields() {
+
+        lanceField.textProperty().addListener((observable, oldValue,  newValue) -> {
+            if (!newValue.matches("\\d*[[\\.,]\\d*]?")) {
+                String[] parts = newValue.split("[,\\.]");
+                String replacement = parts[0].replaceAll("[^\\d]","");
+                for (int i = 1; i < parts.length; i++)
+                    replacement += (i == 1 ? "." : "") + parts[i].replaceAll("[^\\d]","");;
+                ((StringProperty) observable).setValue(replacement);
+            }
+        });
+
+        banheirosField.textProperty().addListener((observable, oldValue,  newValue) -> {
+            if (!newValue.matches("\\d*"))
+                ((StringProperty) observable).setValue(newValue.replaceAll("[^\\d]", ""));
+        });
+
+        quartosField.textProperty().addListener((observable, oldValue,  newValue) -> {
+                if (!newValue.matches("\\d*"))
+                ((StringProperty) observable).setValue(newValue.replaceAll("[^\\d]", ""));
+        });
+
+        areaField.textProperty().addListener((observable, oldValue,  newValue) -> {
+            if (!newValue.matches("\\d*[[\\.,]\\d*]?")) {
+                String[] parts = newValue.split("[,\\.]");
+                String replacement = parts[0].replaceAll("[^\\d]","");
+                for (int i = 1; i < parts.length; i++)
+                    replacement += (i == 1 ? "." : "") + parts[i].replaceAll("[^\\d]","");;
+                ((StringProperty) observable).setValue(replacement);
+            }
+        });
+    }
+
+    private boolean hasValidFloat(String t) {
+        try {
+            Float.parseFloat(t);
+            return !t.isEmpty();
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 
     public void setOwner(Usuario user) {
@@ -109,6 +221,7 @@ public class CriarLotePaneController {
             t = new Imovel();
             ((Imovel) t).setArea(Float.valueOf(areaField.getText()));
             ((Imovel) t).setNumeroBanheiros(Integer.valueOf(banheirosField.getText()));
+            ((Imovel) t).setTipo((Imovel.Tipo) subtipoButton.getToggleGroup().getSelectedToggle().getUserData());
             if (subtipoButton.getToggleGroup().getSelectedToggle().getUserData().equals(Imovel.Tipo.Residencial))
                 ((Imovel) t).setNumeroQuartos(Integer.valueOf(quartosField.getText()));
         } else
